b1f4ca5832cbe971d5681eaf51a6de9d20b76f86
kadeguilherme <kadeguilherme@gmail.com>
2022-09-16 23:06:40 -0300

    feat: Trabalho inicial
    Signed-off-by: kadeguilherme <kadeguilherme@gmail.com>
    Co-authored-by: Luiz Aoqui <lgfa29@gmail.com>
    Co-authored-by: Danilo F Rocha <snifbr@gmail.com>
    Co-authored-by: Marcelo Paglione <marcelo@link-info.com>

--- main.tf
+++ main.tf
@@ -1,18 +1,71 @@
-data "hashicups_coffees" "all" {}
+provider "google" {
+  project = "mentoriaiac-20220916"
+  region  = "us-central1"
+}
 
 locals {
-  coffee_name_id_map = { for coffee in data.hashicups_coffees.all.coffees : coffee.name => coffee.id }
+  subnets = [data.google_compute_subnetwork.load-balance, data.google_compute_subnetwork.nomad]
 }
 
-resource "hashicups_order" "order" {
-  dynamic "items" {
-    for_each = var.order
-    content {
-      coffee {
-        id = local.coffee_name_id_map[items.key]
-      }
-      quantity = items.value
-    }
+data "google_compute_network" "vpc-network" {
+  name = "vpc-network"
+}
+
+data "google_compute_subnetwork" "load-balance" {
+  name = "load-balance"
+}
+
+data "google_compute_subnetwork" "nomad" {
+  name = "nomad"
+}
+
+resource "google_compute_firewall" "load-balance" {
+  name          = "load-balance"
+  network       = data.google_compute_network.vpc-network.name
+  source_ranges = ["0.0.0.0/0"]
+  // destination_ranges = [data.google_compute_subnetwork.load-balance.ip_cidr_range]
+  target_tags = ["load-balance"]
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "22"]
   }
+
 }
 
+module "compute_gcp" {
+  source         = "github.com/mentoriaiac/iac-modulo-compute-gcp"
+  project        = "mentoriaiac-20220916"
+  instance_name  = "nginx-0"
+  instance_image = "nginx-20220917004516"
+  machine_type   = "e2-small"
+  zone           = "us-central1-a"
+  network        = data.google_compute_network.vpc-network.name
+  subnetwork     = local.subnets[0].name
+
+
+  labels = {
+    value = "key"
+  }
+
+  tags = ["load-balance"]
+
+}
+
+module "nomad" {
+  source         = "github.com/mentoriaiac/iac-modulo-compute-gcp"
+  project        = "mentoriaiac-20220916"
+  instance_name  = "nginx-1"
+  instance_image = "nginx-20220917004516"
+  machine_type   = "e2-small"
+  zone           = "us-central1-a"
+  network        = data.google_compute_network.vpc-network.name
+  subnetwork     = local.subnets[1].name
+  public_ip      = "ephemeral"
+  labels = {
+    value = "key"
+  }
+
+
+  tags = []
+
+}
